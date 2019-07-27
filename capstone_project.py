df86e361a6c5de2912cc1a400ebf83792a3cd469
akshaymodi0806 <53369473+akshaymodi0806@users.noreply.github.com>
2019-07-27 16:52:54 +0530

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# This notebook will be mainly used for capstone project under IBM Profession Data Science course on Coursera.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
